ac3217713b0c71e2330ddd7ae85a7e9effe12bfc
Roberto Macedo <robertomacedo3007@gmail.com>
2023-05-01 14:00:03 -0300

    Políticas de acesso para Api

--- Backend/ApiCuidar/Program.cs
+++ Backend/ApiCuidar/Program.cs
@@ -8,10 +8,11 @@ using Microsoft.AspNetCore.Localization;
 using System.Globalization;
 
 
-
-
-
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options => options.AddPolicy("AllowLocalhost", policy =>{
+    policy.WithOrigins("http://localhost:5500","http:/127.0.0.1:5500").SetIsOriginAllowed(isOriginAllowed => true).AllowAnyHeader().AllowAnyMethod();
+}));
 
 
 var culture = new CultureInfo("pt-BR");
@@ -48,6 +49,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
+app.UseCors("AllowLocalhost");
 
 app.UseAuthorization();
 
